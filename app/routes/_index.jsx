847d76136c3ae0a0c25d87a2cf8494be56abe628
Lucas Levine <lucaslevine12@gmail.com>
2023-09-01 14:39:36 -0400

    nav back

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -4,12 +4,6 @@ export default function Index() {
   return (
     <div>
       <Nav title="Home Page"></Nav>
-      <div className="min-h-screen flex items-center justify-center bg-cover bg-center" style={{ backgroundImage: 'url("https://d7hftxdivxxvm.cloudfront.net/?quality=80&resize_to=width&src=https%3A%2F%2Fartsy-media-uploads.s3.amazonaws.com%2F2RNK1P0BYVrSCZEy_Sd1Ew%252F3417757448_4a6bdf36ce_o.jpg&width=910")' }}>
-        <div className="text-white text-center">
-          <h1 className="text-4xl font-bold mb-4">Welcome to My Website</h1>
-          <p className="text-lg">This is a centered page with a background image.</p>
-        </div>
-      </div>
     </div>
   );
 }
