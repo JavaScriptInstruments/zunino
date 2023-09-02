654cf45b9413c29c5676d2f75001fa84b0cbe36d
Dylan Wiafe <36554065+dylanwiafe@users.noreply.github.com>
2023-09-02 15:03:43 -0400

    latest changes

--- app/routes/drawing.jsx
+++ app/routes/drawing.jsx
@@ -4,20 +4,40 @@ import Drawing from "../components/Drawing";
 import { getDrawings } from "../lib/WordPressService";
 
 export async function loader() {
-    return await getDrawings();
-  }
+  return await getDrawings();
+}
 
 export default function Drawings() {
-    const drawings = useLoaderData();
+  const drawings = useLoaderData();
 
-    return (
-        <div>
-            <Nav title="Home Page"></Nav>
-            <div className="grid gap-8 grid-cols-1 lg:grid-cols-3 p-6">
+  return (
+    <div>
+      <Nav title="Home Page"></Nav>
+      {/* <div className="grid gap-8 grid-cols-1 lg:grid-cols-3 p-6">
             {drawings.map((drawing) => {
                 return <Drawing drawing={drawing} key={drawing.name}></Drawing>;
             })}
-            </div>
+            </div> */}
+      <div className="bg-white">
+        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+          <h2 className="sr-only">Products</h2>
+
+          <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+            {drawings.map((drawing) => (
+              <a key={drawing.name} className="group">
+                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+                  <img
+                    src={drawing.photo.sourceUrl}
+                    // alt={product.imageAlt}
+                    className="h-full w-full object-cover object-center group-hover:opacity-75"
+                  />
+                </div>
+                <h3 className="mt-4 text-sm text-gray-700">{drawing.name}</h3>
+              </a>
+            ))}
+          </div>
         </div>
-    );
-  }
+      </div>
+    </div>
+  );
+}
