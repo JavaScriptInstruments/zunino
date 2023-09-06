334679ed8d0d84471d3aaba5e66fac9dd19f40db
Dylan Wiafe <36554065+dylanwiafe@users.noreply.github.com>
2023-09-05 20:47:41 -0400

    photo section

--- app/components/Drawing.jsx
+++ app/components/Drawing.jsx
@@ -3,23 +3,12 @@ import { Link } from "@remix-run/react";
 export default function Drawing({ drawing }) {
   return (
     <Link prefetch="render" to={`/drawings/${drawing.slug}`}>
-      <div
-      // className="flex
-      //     items-center
-      //     bg-gradient-to-r
-      //     p-8
-      //     rounded-lg
-      //     text-black
-      //     transition-all
-      //     hover:-translate-y-1
-      //     hover:scale-105"
-      >
-        <div>
+      <div className="relative">
+        <div className="h-full">
           <img
             src={drawing.photo.sourceUrl}
-            className="h-full w-full object-cover object-center group-hover:opacity-75"
+            className="h-full object-auto object-center group-hover:opacity-75"
           />
-          <h2 className="mt-4 text-sm text-gray-700">{drawing.name}</h2>
         </div>
       </div>
     </Link>
--- app/routes/drawing.jsx
+++ app/routes/drawing.jsx
@@ -15,28 +15,17 @@ export default function Drawings() {
       <Nav title="Home Page"></Nav>
       <div className="bg-white">
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-          {/* <h2 className="sr-only">Products</h2> */}
-
+          <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl mb-10">
+            Drawings{" "}
+          </h1>
           <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-            {/* {drawings.map((drawing) => (
-              <a key={drawing.name} className="group">
-                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-                  <img
-                    src={drawing.photo.sourceUrl}
-                    // alt={product.imageAlt}
-                    className="h-full w-full object-cover object-center group-hover:opacity-75"
-                  />
-                </div>
-                <h3 className="mt-4 text-sm text-gray-700">{drawing.name}</h3>
-              </a>
-              
-            ))} */}
             {drawings.map((drawing) => {
               return (
-                <a key={drawing.name} className="group">
-                  <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+                <a key={drawing.name} className="group ">
+                  <div className="aspect-h-1 h-full w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-5 xl:aspect-w-7 ">
                     <Drawing drawing={drawing} key={drawing.name}></Drawing>
                   </div>
+                  <h2 className="mt-2 text-sm text-gray-700">{drawing.name}</h2>
                 </a>
               );
             })}
